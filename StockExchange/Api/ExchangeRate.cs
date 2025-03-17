e34af29e488c673e0d7357a70427118817fb192d
NaNlaggergit <99110272+NaNlaggergit@users.noreply.github.com>
2025-03-17 18:39:18 +0700

    Добавления изображений приложения

--- StockExchange/Api/ExchangeRate.cs
+++ StockExchange/Api/ExchangeRate.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace StockExchange.Api
 {
+    // Класс для обобщения данных из разных бирж 
     internal class ExchangeRate
     {
         public decimal? LastPrice { get; set; }
